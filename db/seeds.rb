61280639f8d54d940bdd99814e523b6c1d109380
Baylor Rae <baylor@thecodedeli.com>
2017-04-02 21:25:56 -0500

    add devise user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.find_or_create_by!(email: 'demo@example.com') do |user|
+  user.password = 'secret'
+end
