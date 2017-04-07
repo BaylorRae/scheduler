ca0a84fa03e5a6205423176b7e2a0e874b9e3e27
Baylor Rae <baylor@thecodedeli.com>
2017-04-06 22:02:51 -0500

    add options for hourly and other frequencies

--- client/src/app/job-form/job-form.component.ts
+++ client/src/app/job-form/job-form.component.ts
@@ -1,5 +1,16 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
+const padNumber = (num) => num < 10 ? `0${num}` : num;
+
+const DAILY_OPTIONS = Array.from(Array(24))
+  .map((x, i) => padNumber(i)) 
+  .map(i => [i + ':00', i + ':30'])
+  .reduce((a, i) => { a.push(...i); return a }, []);
+
+const HOURLY_OPTIONS = [0, 10, 20, 30, 40, 50]
+  .map(i => padNumber(i))
+  .map(i => '00:' + i);
+
 @Component({
   selector: 'job-form',
   templateUrl: './job-form.component.html',
@@ -11,11 +22,17 @@ export class JobFormComponent implements OnInit {
   @Output() cancel = new EventEmitter();
 
   model: any = {};
+  runAtOptions: string[] = DAILY_OPTIONS;
 
   constructor() { }
 
   ngOnInit() {
     this.model = this.job || {};
+    this.changeFrequency(this.model.frequency);
+  }
+
+  changeFrequency(frequency) {
+    this.runAtOptions = frequency === 'hourly' ? HOURLY_OPTIONS : DAILY_OPTIONS;
   }
 
   cancelEvent() {
