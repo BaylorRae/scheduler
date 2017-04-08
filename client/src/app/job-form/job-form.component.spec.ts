032af5d55022e1cc79e0a6d5c61549e6376ddd76
Baylor Rae <baylor@thecodedeli.com>
2017-04-08 11:54:27 -0500

    add tests for cancel and save callbacks

--- client/src/app/job-form/job-form.component.spec.ts
+++ client/src/app/job-form/job-form.component.spec.ts
@@ -27,4 +27,35 @@ describe('JobFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('cancelEvent', () => {
+    it('calls subscribers to the cancel event', () => {
+      const cancelCallback = jasmine.createSpy('cancel');
+      component.cancel.subscribe(cancelCallback);
+      component.cancelEvent();
+      expect(cancelCallback).toHaveBeenCalled();
+    });
+  });
+
+  describe('saveEvent', () => {
+    const saveCallback = jasmine.createSpy('save');
+
+    beforeEach(() => {
+      component.save.subscribe(saveCallback);
+    });
+
+    it('calls subscribers to the save event', () => {
+      component.saveEvent();
+      expect(saveCallback).toHaveBeenCalled();
+    });
+
+    it('passes the job object to subscribers', () => {
+      component.model = { param1: 'param-1', param2: 'param-2' };
+      component.saveEvent();
+      expect(saveCallback).toHaveBeenCalledWith({
+        param1: 'param-1',
+        param2: 'param-2'
+      });
+    });
+  });
 });
