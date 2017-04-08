39b437e42a40b4dce59d7b0be4545331338dcc17
Baylor Rae <baylor@thecodedeli.com>
2017-04-08 11:35:02 -0500

    add tests for updating a job

--- client/src/app/job-item/job-item.component.spec.ts
+++ client/src/app/job-item/job-item.component.spec.ts
@@ -15,12 +15,16 @@ describe('JobItemComponent', () => {
   let component: JobItemComponent;
   let fixture: ComponentFixture<JobItemComponent>;
 
+  const updateObservable = {
+    subscribe: jasmine.createSpy('update.subscribe')
+  }
+
   const deleteObservable = {
     subscribe: jasmine.createSpy('delete.subscribe')
   };
 
   const mockJobService = {
-    update: jasmine.createSpy('update'),
+    update: jasmine.createSpy('update').and.returnValue(updateObservable),
     delete: jasmine.createSpy('delete').and.returnValue(deleteObservable)
   }
 
@@ -47,9 +51,48 @@ describe('JobItemComponent', () => {
   });
 
   describe('update', () => {
-    it('saves thes changes made to the job');
-    it('marks editing as false');
-    it('updates the job to the response');
+    it('saves thes changes made to the job', () => {
+      component.job.id = 7331;
+      component.saveJob({ param1: 'param-1', param2: 'param-2' })
+      expect(mockJobService.update).toHaveBeenCalledWith(7331, {
+        param1: 'param-1',
+        param2: 'param-2'
+      });
+    });
+
+    it('subscribes to the update request', () => {
+      component.job.id = 1;
+      component.saveJob({});
+      expect(updateObservable.subscribe).toHaveBeenCalled();
+    });
+
+    it('marks editing as false', () => {
+      let successCallback;
+
+      updateObservable.subscribe = (success) => successCallback = success;
+
+      component.editing = true;
+      component.job.id = 1;
+      component.saveJob({});
+      successCallback();
+
+      expect(component.editing).toBeFalsy();
+    });
+
+    it('updates the job to the response', () => {
+      let successCallback;
+
+      updateObservable.subscribe = (success) => successCallback = success;
+
+      component.job.id = 1;
+      component.saveJob({});
+      successCallback({ param1: 'param-1', param2: 'param-2' });
+
+      expect(component.job).toEqual({
+        param1: 'param-1',
+        param2: 'param-2'
+      });
+    });
   });
 
   describe('delete', () => {
